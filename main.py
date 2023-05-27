f2e05bae066f363ac34ebbba610078636713fc67
barisumut <barisumutbaykal@gmail.com>
2023-05-27 22:27:44 +0300

    Add new insert dialog page, add student name widget, combo box of courses, mobile widget, submit button

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, \
-    QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
 import sqlite3
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, \
+    QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, \
+    QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, \
+    QComboBox
+
+
 
 
 class MainWindow(QMainWindow):
@@ -15,6 +19,7 @@ class MainWindow(QMainWindow):
         help_menu_item = self.menuBar().addMenu("&Help")
 
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -36,6 +41,42 @@ class MainWindow(QMainWindow):
             for column_number, data in enumerate(row_data):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
         connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Add student name widget
+        student_name = QLineEdit()
+        student_name.setPlaceholderText("Name")
+        layout.addWidget(student_name)
+
+        # Add combo box of courses
+        course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        course_name.addItems(courses)
+        layout.addWidget(course_name)
+
+        # Add mobile widget
+        mobile = QLineEdit()
+        mobile.setPlaceholderText("Mobile")
+        layout.addWidget(mobile)
+
+        # Add a submit button
+        button = QPushButton("Register ")
+        button.clicked.connect(self.add_student)
+
+        self.setLayout(layout)
 
 
 
